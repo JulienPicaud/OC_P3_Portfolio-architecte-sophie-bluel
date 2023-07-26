e12ae0bd07e69911622280867382198033e60a46
Julien Picaud <julienpicaud83@gmail.com>
2023-07-26 16:49:00 +0200

    Test de fetch

---
+++ FrontEnd/fetch.js
@@ -0,0 +1,10 @@
+const myImage = document.querySelector('img');
+
+fetch('http://localhost:5678/api/works')
+.then(function(response) {
+  return response.blob();
+})
+.then(function(myBlob) {
+  const objectURL = URL.createObjectURL(myBlob);
+  myImage.src = objectURL;
+});
