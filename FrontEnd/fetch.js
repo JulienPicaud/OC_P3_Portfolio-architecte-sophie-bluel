1bf1d17348778b7a44bfc9129dd0e29d9f0dca87
Julien Picaud <julienpicaud83@gmail.com>
2023-08-01 21:47:53 +0200

    Erreur delete gallery

--- FrontEnd/fetch.js
+++ FrontEnd/fetch.js
@@ -5,50 +5,53 @@ function getProject() {
   };
 
   fetch("http://localhost:5678/api/works", requestOptions)
-    .then((response) => {
-      return response.json()
-    })
-    .then((result) => {
-      console.log(result)
-      deleteGallery()
-      createGallery(result)
-    })
-    .catch(error => console.log('error', error));
+  .then(response => response.json())
+  .then(result => console.log(result))
+  deleteGallery()
+  createGallery(result)
+  .catch(error => console.log('error', error));
 }
 
+
 function deleteGallery() {
-  let gallery = document.querySelector('.gallery');
+  const gallery = document.querySelector('.gallery');
   gallery.remove();
 }
 
-//projects.forEach(project =>{
-//  
-//  const figure = document.createElement('figure');
-//  const img = document.createElement('img');
-//  const figureCaption = document.createElement('figureCaption');
-//  
-//  img.src = project.imageUrl;
-//  img.alt = project.title;
-//  figCaption.textContent = project.title;
-//  
-//  figure.appendChild(img)
-//  figure.appendChild(figureCaption)
-//  gallery.appendChild(figure)  
-//})
 
-
-function createGallery(newGallery) {
-  let gallery = document.createElement('div')
+function createGallery() {
+  result.forEach(project => {
+    const div = document.createElement('div')  
+    const img = document.createElement('img');
+    const figureCaption = document.createElement('figureCaption');
   
-  const img = document.createElement('img');
-  const figCaption = document.createElement('figCaption');
+  
+    img.src = result.imageUrl;
+    img.alt = result.title
+    figureCaption.textContent = result.title;
+  
+  div.appendChild(img)
+  div.appendChild(figureCaption)
+  document.body.appendChild(gallery)
 
-  gallery.appendChild(img)
-  gallery.appendChild(figCaption)
-  .document.body.appendChild(gallery)
+  })
 }
-  
-  getProject()
+
+getProject()
 
 
+
+
+
+    //const figure = document.createElement('figure');
+    //const img = document.createElement('img');
+    //const figureCaption = document.createElement('figureCaption');
+//
+    //img.src = project.imageUrl;
+    //img.alt = project.title;
+    //figureCaption.textContent = project.title;
+//
+    //figure.appendChild(img)
+    //figure.appendChild(figureCaption)
+    //gallery.appendChild(figure)
 
