0992d7e3434b2829b727be641aa3e3171fb6bda6
Julien Picaud <julienpicaud83@gmail.com>
2023-08-01 19:41:02 +0200

    Creation gallerie test

--- FrontEnd/fetch.js
+++ FrontEnd/fetch.js
@@ -1,12 +1,15 @@
 function getProject() {
-  var requestOptions = {
+  let requestOptions = {
     method: 'GET',
     redirect: 'follow'
   };
 
   fetch("http://localhost:5678/api/works", requestOptions)
-    .then(response => response.json())
-    .then(result => {
+    .then((response) => {
+      return response.json()
+    })
+    .then((result) => {
+      console.log(result)
       deleteGallery()
       createGallery(result)
     })
@@ -14,14 +17,38 @@ function getProject() {
 }
 
 function deleteGallery() {
-
+  let gallery = document.querySelector('.gallery');
+  gallery.remove();
 }
 
-function createGallery(projects) {
+//projects.forEach(project =>{
+//  
+//  const figure = document.createElement('figure');
+//  const img = document.createElement('img');
+//  const figureCaption = document.createElement('figureCaption');
+//  
+//  img.src = project.imageUrl;
+//  img.alt = project.title;
+//  figCaption.textContent = project.title;
+//  
+//  figure.appendChild(img)
+//  figure.appendChild(figureCaption)
+//  gallery.appendChild(figure)  
+//})
 
+
+function createGallery(newGallery) {
+  let gallery = document.createElement('div')
+  
+  const img = document.createElement('img');
+  const figCaption = document.createElement('figCaption');
+
+  gallery.appendChild(img)
+  gallery.appendChild(figCaption)
+  .document.body.appendChild(gallery)
 }
+  
+  getProject()
 
 
-
-getProject()
 
