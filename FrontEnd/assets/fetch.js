684e5f6d93d2608b49ceead1ec4c70efb67d7bd5
Julien Picaud <julienpicaud83@gmail.com>
2023-08-18 11:43:19 +0200

    creation boutons dynamique

--- FrontEnd/assets/fetch.js
+++ FrontEnd/assets/fetch.js
@@ -6,13 +6,13 @@ function getProject() {
   };
 
   fetch("http://localhost:5678/api/works", requestOptions)
-  .then(response => response.json())
-  .then((result) => {
-  console.log(result)
-  deleteGallery()
-  createGallery(result)
-})
-  .catch(error => console.log('error', error))
+    .then(response => response.json())
+    .then((result) => {
+      console.log(result)
+      deleteGallery()
+      createGallery(result)
+    })
+    .catch(error => console.log('error', error))
 }
 
 // On supprime la div gallery
@@ -21,32 +21,31 @@ function deleteGallery() {
   gallery.remove();
 }
 
+
 // On recré la gallerie
 function createGallery(projects) {
-  let gallery = document.createElement('div'); 
+  let gallery = document.createElement('div');
   let portfolio = document.getElementById('portfolio')
   gallery.setAttribute('class', 'gallery');
   portfolio.appendChild(gallery)
-  
-
-  function attributeProject() {
   projects.forEach((project) => {
     const figure = document.createElement('figure')
     const img = document.createElement('img');
     const figureCaption = document.createElement('figureCaption');
-  
+
     img.src = project.imageUrl;
     img.alt = project.title
     figureCaption.textContent = project.title;
-  
-  figure.appendChild(img)
-  figure.appendChild(figureCaption)
-  gallery.appendChild(figure)
 
-  })}
-  attributeProject()
+    figure.appendChild(img)
+    figure.appendChild(figureCaption)
+    gallery.appendChild(figure)
+
+  }) 
+
 }
-getProject()
+function attributeProject() {
+}
 
 /////////////////////////////////////////////////////
 /////////////////////////////////////////////////////
@@ -58,39 +57,63 @@ getProject()
 function getCategories() {
   // Fetch les categories sur l'API
   let requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
+    method: 'GET',
+    redirect: 'follow'
   };
 
   fetch("http://localhost:5678/api/categories", requestOptions)
-      .then(response => response.json())
-      .then((filters) => {
-          deleteGallery()
-          createFilters(filters)
-      })
-      .catch(error => console.log('error', error))
+    .then(response => response.json())
+    .then((filters) => {
+      const monSet = new Set(filters);
+      console.log(monSet)
+      createButton(monSet)
+
+      //createFilters(filters)
+    })
+    .catch(error => console.log('error', error))
 }
 
-function createFilters(filters) {
+function createButton(filters){
+
+let filtersButton = document.getElementById('filtersButton');
+
+let resetButton = document.createElement('input');
+resetButton.setAttribute('class', 'resetButton')
+resetButton.type ='button'
+resetButton.value = "Réinitialiser"
+filtersButton.appendChild(resetButton)
+
+  filters.forEach((filter) => {
+  let input = document.createElement('input');
+  input.setAttribute('class', 'button')
+  filtersButton.appendChild(input)
+
+  input.value = filter.name
+  input.type = 'button'
+})
+}
+
+
+function createFilters(filters) {  
   let gallery = document.createElement('div');
   let portfolio = document.getElementById('portfolio')
   gallery.setAttribute('class', 'gallery');
   portfolio.appendChild(gallery)
 
   filters.forEach((filter) => {
-      const figure = document.createElement('figure')
-      const img = document.createElement('img');
-      const figureCaption = document.createElement('figureCaption');
+    const figure = document.createElement('figure')
+    const img = document.createElement('img');
+    const figureCaption = document.createElement('figureCaption');
 
-      img.src = filter.imageUrl;
-      img.alt = filter.title
-      figureCaption.textContent = filter.title;
+    img.src = filter.imageUrl;
+    img.alt = filter.title
+    figureCaption.textContent = filter.title;
 
-      figure.appendChild(img)
-      figure.appendChild(figureCaption)
-      gallery.appendChild(figure)
+    figure.appendChild(img)
+    figure.appendChild(figureCaption)
+    gallery.appendChild(figure)
 
   })
 }
-
+getProject()
 getCategories()
