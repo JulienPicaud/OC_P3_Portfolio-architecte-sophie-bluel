483f35007b0b7652341b87ced1eec764146b2854
Julien Picaud <julienpicaud83@gmail.com>
2023-08-25 09:39:39 +0200

    fichier js mis au propre

--- FrontEnd/assets/Filtres.js
+++ FrontEnd/assets/Filtres.js
@@ -4,7 +4,6 @@ let requestOptions = {
     method: 'GET',
     redirect: 'follow'
 };
-// cette fonction joue fetch, supprime la galerie puis en recréée une
 
 /**
  * get all pictures and display
@@ -14,7 +13,7 @@ function getProject() {
         .then(response => response.json())
         .then((result) => {
             projectsList = result
-            niceGallery(projectsList)
+            resetGallery(projectsList)
         })
         .catch(error => console.log('error', error))
 }
@@ -45,7 +44,7 @@ function deleteGallery() {
 /**
  * reset gallery
  */
-function niceGallery() {
+function resetGallery() {
     deleteGallery()
     createGallery(projectsList)
 }
@@ -53,65 +52,32 @@ function niceGallery() {
 // On recré la div gallery
 function createGallery(projectsList) {
     let portfolio = document.getElementById('portfolio')
-
-    // let gallery = document.createElement('div');
-    // gallery.setAttribute('class', 'gallery');
-    // portfolio.appendChild(gallery)
     let gallery = createMarkup('div', portfolio, { class: 'gallery' })
-
     projectsList.forEach((project) => {
-        // const figure = document.createElement('figure')
-        // const img = document.createElement('img');
-        // const figureCaption = document.createElement('figureCaption');
         const figure = createMarkup('figure', gallery);
         createMarkup('img', figure, { src: project.imageUrl, alt: project.title });
-        const figureCaption = createMarkup('figureCaption', figure, {}, project.title);
-        // figureCaption.textContent = project.title;
-
-        // img.src = project.imageUrl;
-        // img.alt = project.title
-
-        // figure.appendChild(img)
-        // figure.appendChild(figureCaption)
-        // gallery.appendChild(figure)
-
+        createMarkup('figureCaption', figure, {}, project.title);
     })
-
 }
-
 
 /**
  * creates the buttons according to fetched categories
  * @param {set} categoriesList 
  */
+
 function createButton(categoriesList) {
-
     let filtersButton = document.getElementById('filtersButton');
-
-    // let resetButton = document.createElement('input');
-    // resetButton.setAttribute('class', 'resetButton')
-    // resetButton.type = 'button'
-    // resetButton.value = "Réinitialiser"
-    // filtersButton.appendChild(resetButton)
     let resetButton = createMarkup('input', filtersButton, { value: 'Tous', type: 'button', class: 'button' })
     resetButton.addEventListener('click', () => {
-        niceGallery();
+        resetGallery();
     })
     categoriesList.forEach((category) => {
-
-        // let input = document.createElement('input');
-        // input.setAttribute('class', 'button')
-        // filtersButton.appendChild(input)
-        // input.value = category.name
-        // input.type = 'button'
         let input = createMarkup('input', filtersButton, { value: category.name, type: 'button', class: 'button' })
-
         input.addEventListener("click", () => {
             filterObject(category.id)
         })
     })
 }
-
 
 /**
  * create a tag and bind it to the DOM
@@ -122,53 +88,30 @@ function createButton(categoriesList) {
  */
 function createMarkup(tag, parent, attributes = {}, content = '') {
     let htmlElement = document.createElement(tag);
-    htmlElement.innerHTML = content;
+    parent.appendChild(htmlElement);
     if (Object.keys(attributes).length > 0) {
         for (const [key, value] of Object.entries(attributes)) {
             htmlElement.setAttribute(key, value);
         }
     }
-    parent.appendChild(htmlElement);
+    htmlElement.innerHTML = content;
     return htmlElement;
 }
 
-
-
 /**
  * clean the last gallery and display filtered elements
- * @param {set} categoriesList 
  */
 function filterObject(categoriesList) {
-    //   deleteGallery()
-    //   let gallery = document.createElement('div');
-    //   let portfolio = document.getElementById('portfolio')
-    //   gallery.setAttribute('class', 'gallery');
-    //   portfolio.appendChild(gallery)
     let gallery = document.querySelector('.gallery');
     gallery.innerHTML = ''
 
-    let filteredEntities = projectsList.filter((p) => {
+    let filteredProjects = projectsList.filter((p) => {
         return p.category.id === categoriesList
     })
-
-    filteredEntities.forEach((objets) => {
-
+    filteredProjects.forEach((objets) => {
         const figure = createMarkup('figure', gallery);
         createMarkup('img', figure, { src: objets.imageUrl, alt: objets.title });
-        const figureCaption = createMarkup('figureCaption', figure, {}, objets.title);
-
-        //  const figure = document.createElement('figure')
-        //  const img = document.createElement('img');
-        //   const figureCaption = document.createElement('figureCaption');
-
-        //  img.src = objets.imageUrl;
-        //    img.alt = objets.title
-        //   figureCaption.textContent = objets.title;
-
-        //   figure.appendChild(img)
-        //  figure.appendChild(figureCaption)
-        //   gallery.appendChild(figure)
-
+        createMarkup('figureCaption', figure, {}, objets.title);
     })
 }
 
