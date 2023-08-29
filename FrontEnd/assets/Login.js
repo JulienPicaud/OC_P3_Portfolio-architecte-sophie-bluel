a7fd64c73be3eda1e86c29c933166de4017ccf20
Julien Picaud <julienpicaud83@gmail.com>
2023-08-29 21:12:13 +0200

    mise en place du login.js

---
+++ FrontEnd/assets/Login.js
@@ -0,0 +1,10 @@
+let requestOptions = {
+    method: 'POST',
+    redirect: 'follow'
+};
+
+fetch("http://localhost:5678/api/users/login", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
