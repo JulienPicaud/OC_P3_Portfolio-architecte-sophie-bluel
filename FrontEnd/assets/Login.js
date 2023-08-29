8261d4244ccd8503283bafcc8d816b1913377d3f
Julien Picaud <julienpicaud83@gmail.com>
2023-08-29 22:19:02 +0200

    preparation login.js

--- FrontEnd/assets/Login.js
+++ FrontEnd/assets/Login.js
@@ -2,10 +2,11 @@ let requestOptions = {
     method: 'POST',
     redirect: 'follow'
 };
+let connectButton = document.getElementById('connectButton')
+
 
 fetch("http://localhost:5678/api/users/login", requestOptions)
     .then(response => response.json())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
 
-let connectButton = document.getElementById('connectButton')
