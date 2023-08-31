b148d9dba14dd9333aa39c118ae8ee9cae5f0b29
Julien Picaud <julienpicaud83@gmail.com>
2023-08-31 20:59:05 +0200

    fetch post test

--- FrontEnd/assets/Login.js
+++ FrontEnd/assets/Login.js
@@ -1,12 +1,25 @@
+let connectButton = document.getElementById('connectButton')
+connectButton.addEventListener('click', () => {
+    let myInput = document.querySelector('inputLog');
+    console.log(myInput.value)
+}) 
+
+let formdata = new FormData(connectButton);
+formdata.append("email", "string");
+formdata.append("password", "string");
+
 let requestOptions = {
     method: 'POST',
+    body: formdata,
     redirect: 'follow'
 };
-let connectButton = document.getElementById('connectButton')
-
 
 fetch("http://localhost:5678/api/users/login", requestOptions)
-    .then(response => response.json())
+    .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
+
+
+
+
 
